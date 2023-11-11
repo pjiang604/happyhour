10e98786c3008531b2c2269e88e628119d75116e
pjiang604 <patriciaejiang@gmail.com>
2023-11-10 19:50:39 -0800

    added api and basic mapping of all drinks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,44 @@
-import { Outlet, Link } from "react-router-dom";
 import Footer from "../components/Footer";
+import "./Home.css"
+import { useState, useEffect } from "react";
 
 const Home = () => {
+
+  const [data, setData] = useState();
+
+  const API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail';
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(response => response.json()) //converts the data to json file
+      .then(response => {
+        console.log(response);
+        setData(response); //the data is now stored in the useState
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
   return (
     <>
-    <h2>this is on the home page</h2>
+      <div className="contentContainer">
+        <h2>this is on the home page</h2>
+        <div className="drinkDisplay">
+          {data && data.drinks.map((i, index) => {
+            return (
+              <div
+              className="drinkCard"
+                key={index}>
+                <img src={i.strDrinkThumb} className="drinkImg" />
+                {i.strDrink}
+              </div>
+            )
+          })}
+        </div>
+      </div>
 
-      <Outlet />
-      <Footer/>
+      <Footer />
     </>
   )
 };
