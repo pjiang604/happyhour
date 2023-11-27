8fb51014fa2998974ca1c9f55086bf21572033dc
Ivona <ivona.i.work@gmail.com>
2023-11-26 23:54:33 -0800

    notes edit

--- src/components/Notes/index.js
+++ src/components/Notes/index.js
@@ -1,18 +1,37 @@
 import './style.css';
-export default function Notes() {
+import { useState, useEffect } from 'react';
+export default function Notes(props) {
+    const [edit, setEdit] = useState(false);
+    const note = props.note;
+    const [notes, setNotes] = useState([]);
+//   function handleDelete() {
+//     props.remove(note);
+//   }
+  function handleStatusChange() {
+    props.toggleChecked(note);
+  }
+  function handleEdit() {
+    setEdit(!edit);
+  }
+  const handleAdd = () => {
+    const abc = [...notes, []];
+    setNotes(abc);
+  };
+  
     return(
         <>
-        <div className='inputs'>
+        <li className='inputs'>
             <input type='date' className="date_container"/>
             <div className='input_container'>
             <textarea placeholder='Write something ...' className='notes_container' />
             <div className='buttons'>
-            <button className='edit_button'>Edit</button>
-            <button className='delete_button'>Delete</button>
+            <button onClick={handleEdit} className='edit_button'>Edit</button>
+            <button onClick={handleDelete} className='delete_button'>Delete</button>
+            <button onClick={() => handleAdd()}>Add Note</button>
             </div>
             
             </div>
-        </div>
+        </li>
         </>
     )
 }
